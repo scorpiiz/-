7fc08c70da48b1a71836967ec570fd43da4dad09
scorpiiz <156436256+scorpiiz@users.noreply.github.com>
2024-03-06 10:49:38 +0800

    Add files via upload
    Signed-off-by: scorpiiz <156436256+scorpiiz@users.noreply.github.com>

---
+++ 辅助材料/我的领养界面 2024.3.6/index.js
@@ -0,0 +1,38 @@
+const wxcom = require('../../utils/common.js')
+
+const app=getApp()
+Page({
+  data: {
+    list: [],
+
+    isShowlogin:false,
+	isload:false,
+  },
+  //页面加载
+  onLoad: function () {
+    wxcom.yzlogin();
+    this.getlist();
+  },
+  //获取数据
+  getlist: function () {
+     var that = this
+     let para= {f:4,cmd:'adopt_List'};
+      wxcom.loaddata(para,function(res){
+	        that.setData({
+          list: res.data.data
+		})
+	  });
+  },
+  //页面跳转
+  gonav:function(e){
+    wx.navigateTo({ url: e.currentTarget.dataset.n })
+  },
+  
+  gofav(e) {
+    const params ={};
+    params.cmd=e.currentTarget.dataset.n;
+    params.it=e.currentTarget.dataset.it;
+	params.zt=e.currentTarget.dataset.f;
+    wxcom.savedata(params,this.getlist);
+  }
+})
