10199e406bf02b222e6e89d2df6439b3d2173bbd
scorpiiz <156436256+scorpiiz@users.noreply.github.com>
2024-03-19 17:42:59 +0800

    Add files via upload
    Signed-off-by: scorpiiz <156436256+scorpiiz@users.noreply.github.com>

---
+++ 辅助材料/2024.3.19/mypetlost/index.js
@@ -0,0 +1,39 @@
+const wxcom = require('../../utils/common.js')
+
+const app=getApp()
+Page({
+  data: {
+    list: [],
+
+    isShowlogin:false,
+	isload:false,
+  },
+  //页面加载
+  onLoad: function () {
+
+    this.getlist();
+
+  },
+  //获取数据
+  getlist: function () {
+     var that = this
+     let para={f:4,cmd:'petlost_List'};
+      wxcom.loaddata(para,function(res){
+	        that.setData({
+          list: res.data.data
+		})
+	  });
+  },
+  //页面跳转
+  gonav:function(e){
+    wx.navigateTo({ url: e.currentTarget.dataset.n })
+  },
+  
+  gofav(e) {
+    const params ={};
+    params.cmd=e.currentTarget.dataset.n;
+
+    params.f=1;
+    wxcom.savedata(params,this.getlist);
+  }
+})
